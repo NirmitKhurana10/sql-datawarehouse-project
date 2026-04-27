ac8b4d404785f85e74799bee503e7034510b01d6
NirmitKhrurana10 <nkhurana1010@gmail.com>
2026-04-26 20:34:40 -0400

    find bronze layer errors

--- scripts/silver/find_bronze_layer_errors.sql
+++ scripts/silver/find_bronze_layer_errors.sql
@@ -1,5 +1,10 @@
+-- ===================================== crm_cst_info ===========================================
+
+
+
 -- Check for null and duplicates in Primary Key
 -- Expectation: No Result
+
 
 
 select 
@@ -36,3 +41,22 @@ select DISTINCT cci.cst_gndr from og_bronze.crm_cust_info cci
 
 select DISTINCT cci.cst_marital_status from og_bronze.crm_cust_info cci 
 
+
+
+-- ===================================== crm_prd_info ===========================================
+
+
+-- Check for null and duplicates in Primary Key
+-- Expectation: No Result
+
+
+select 
+	cpi.prd_id, 
+	count(*) as 'duplicate count' 
+from 
+	og_bronze.crm_prd_info cpi 
+	group by cpi.prd_id 
+	having count(*) > 1 or cpi.prd_id = 0;
+
+
+SELECT * from og_bronze.crm_cust_info cci where cci.cst_id = 0
