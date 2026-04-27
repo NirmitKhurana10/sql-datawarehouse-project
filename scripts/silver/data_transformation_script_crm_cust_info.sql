4d8a17d104e9327700708f9f0ed6a5b2c3768942
NirmitKhrurana10 <nkhurana1010@gmail.com>
2026-04-27 11:05:50 -0400

    added comments to transformation script of crm_prd_info

--- scripts/silver/data_transformation_script_crm_cust_info.sql
+++ scripts/silver/data_transformation_script_crm_cust_info.sql
@@ -1,42 +1,34 @@
--- Data Transformation Script
--- Data Transformation Script
-
-INSERT into og_silver.crm_cust_info (
-	cst_id,
-	cst_key,
-	cst_create_date,
-	cst_firstname,
-	cst_lastname,
-	cst_marital_status,
-	cst_gndr
+insert into og_silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+	
 )
 
-
 select
-	t.cst_id,
-	t.cst_key,
-	t.cst_create_date,
-	TRIM(t.cst_firstname) AS 'First_Name', 
-	TRIM(t.cst_lastname) AS 'Last_Name',
-	CASE WHEN UPPER(TRIM(t.cst_marital_status)) = 'S' then 'Single'
-		 WHEN UPPER(TRIM(t.cst_marital_status )) = 'M' then 'Married'
-		 else 'N/A' -- Handle Missing Value
-	END as 'Marital_Status', -- Data Normalization / Standardization
-	CASE WHEN UPPER(TRIM(t.cst_gndr)) = 'F' then 'Female'
-		 WHEN UPPER(TRIM(t.cst_gndr)) = 'M' then 'Male'
-		 else 'N/A' -- 
-	END as 'Gender'-- Data Normalization / Standardization
-from (
-
-	select *, 
-		row_number() over (PARTITION BY cci.cst_id order by cci.cst_create_date desc) as flag_last 
-	from 
-		og_bronze.crm_cust_info cci 
-	where 
-		cci.cst_id != 0
-)t where t.flag_last = 1 -- Selecting the most recent record per customer
-
-
-
-
-
+	cpi.prd_id,
+	-- Deriving new columns
+	REPLACE(SUBSTRING(cpi.prd_key, 1,5),'-','_')as cat_id, -- Extract category id
+	SUBSTRING(cpi.prd_key, 7, LENGTH(cpi.prd_key)) as prd_key, -- Extract Product Key
+	cpi.prd_nm,
+	case when cpi.prd_cost = '' then 0 
+		 else cpi.prd_cost
+		end as prd_cost, -- instead of empty space, we want to have a 0
+	case upper(trim(cpi.prd_line)) 
+		 when 'M' then 'Mountain' 
+		 when 'R' then 'Road'	
+		 when 'S' then 'Other Sales'
+		 when 'T' then 'Touring'
+		 else 'N/A' -- Data normalization and standardization
+		end as prd_line,
+-- 	here we take the last start date of the next record make it the end date of the previous record
+--  also we subtract 1 day from end date to make sure that end date of older price is smaller than start date of new price
+--  i.e end date = start date of new record - 1		
+	CAST(cpi.prd_start_dt as DATE) as prd_start_dt, -- typecasting 
+	CAST(LEAD(cpi.prd_start_dt) over (Partition by cpi.prd_key order by cpi.prd_start_dt) - interval 1 day as DATE) as prd_end_dt -- data enrichment + typecasting
+from og_bronze.crm_prd_info cpi 
