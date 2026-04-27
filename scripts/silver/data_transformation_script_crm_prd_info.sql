b8aaa164e15653a457acd60ba5fa5a532afd6e53
NirmitKhrurana10 <nkhurana1010@gmail.com>
2026-04-27 11:01:13 -0400

    added silver later tranforation script for crm_prd_info

---
+++ scripts/silver/data_transformation_script_crm_prd_info.sql
@@ -0,0 +1,33 @@
+insert into og_silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+	
+)
+
+select
+	cpi.prd_id,
+	REPLACE(SUBSTRING(cpi.prd_key, 1,5),'-','_')as cat_id,
+	SUBSTRING(cpi.prd_key, 7, LENGTH(cpi.prd_key)) as prd_key,
+	cpi.prd_nm,
+	case when cpi.prd_cost = '' then 0 
+		 else cpi.prd_cost
+		end as prd_cost,
+	case upper(trim(cpi.prd_line)) 
+		 when 'M' then 'Mountain' 
+		 when 'R' then 'Road'	
+		 when 'S' then 'Other Sales'
+		 when 'T' then 'Touring'
+		 else 'N/A'
+		end as prd_line,
+-- 	here we take the last start date of the next record make it the end date of the previous record
+--  also we subtract 1 day from end date to make sure that end date of older price is smaller than start date of new price
+--  i.e end date = start date of new record - 1		
+	CAST(cpi.prd_start_dt as DATE) as prd_start_dt,
+	CAST(LEAD(cpi.prd_start_dt) over (Partition by cpi.prd_key order by cpi.prd_start_dt) - interval 1 day as DATE) as prd_end_dt
+from og_bronze.crm_prd_info cpi 
